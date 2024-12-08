762ab505221b740a5a632f6d870e858e2bb4adc6
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-08 12:43:40 -0600

    Use scan function to get user inputs

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	investmentAmount, years := 1000.0, 10.0
+	var investmentAmount float64
+	years := 10.0
 	expectedReturnRate := 5.5
+
+	fmt.Print("Introduce the Investment amount: ")
+	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount *
 		math.Pow(1+expectedReturnRate/100, years)
