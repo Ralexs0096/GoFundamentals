60fa082fd803895af9407c59dfacf1e1a155941a
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-10 17:16:18 -0600

    Get all the values from the user

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,11 +8,17 @@ import (
 func main() {
 	const inflationRate = 2.5
 	var investmentAmount float64
-	years := 10.0
-	expectedReturnRate := 5.5
+	var years float64
+	var expectedReturnRate float64
 
 	fmt.Print("Introduce the Investment amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount *
 		math.Pow(1+expectedReturnRate/100, years)
